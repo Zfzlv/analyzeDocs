097d300eb40866f1a2a31da7bf20d3fc0926b6a3
Jacob.Lee <Zfzlv@users.noreply.github.com>
2017-07-19 16:57:44 +0800

    完整版
    using java

---
+++ src/com/mycompany/analyzedoc/util/TestFileType.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.analyzedoc.util;
+
+import java.io.File;
+import javax.activation.MimetypesFileTypeMap;
+import org.apache.commons.io.FileUtils;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestFileType {
+    public static void main(String[] args) {
+        File f=new File("D:\\zxxk_test\\document\\7421");
+        
+    }
+}
